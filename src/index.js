68c907e6b33e90fc8c87c6efaa9fc71e7de3792e
Jefferson LE QUELLEC <jle-quel@e2r7p21.42.fr>
2018-02-26 18:43:25 +0100

    Editline [OK]

--- src/index.js
+++ src/index.js
@@ -6,17 +6,38 @@
 /*   By: jle-quel <jle-quel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 18:02:49 by jle-quel          #+#    #+#             */
-/*   Updated: 2018/02/26 18:26:40 by jle-quel         ###   ########.fr       */
+/*   Updated: 2018/02/26 18:42:49 by jle-quel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 "use strict"
 
-const readline = require("readline")
+const readline = require('readline')
 
-readline.emitKeypressEvents(process.stdin)
-process.stdin.setRawMode(true)
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Taskmaster> "
+})
 
-process.stdin.on("keypress", (char) => {
-	if (char === 'E') process.exit(42)
+rl.prompt()
+
+rl.on('line', (line) => {
+	const trimmedLine = line.trim()
+
+	switch trimmedLine {
+		case: "status"
+			console.log("Status")
+		case: "start"
+			console.log("Status")
+		case: "stop"
+			console.log("Stop")
+		case: "restart"
+			console.log("Restart")
+		case: "reload"
+			console.log("Reload")
+		case: "shutdown"
+			console.log("Shutdown")
+	}
+	rl.prompt()
 })
