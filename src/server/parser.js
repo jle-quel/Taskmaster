177e1a1541f2daad5b9c21f03c842688e2b275d8
micka <mickaelmazouz@gmail.com>
2018-03-05 22:58:15 +0100

    add joi parser

--- src/server/parser.js
+++ src/server/parser.js
@@ -1,38 +1,41 @@
 const jsonfile = require('jsonfile')
+const joi = require('joi')
+
+const signalCode = require('../signal-codes')
+
+const envSchema = {"TEST": 'lol'}
+
+const schema = joi.object().keys({
+	command: joi.string().required(),
+	numprocs: joi.number().integer().min(1).default(1),
+	autostart: joi.boolean().default(true),
+	stopwaitsecs: joi.number().integer().min(0).default(10),
+	stdout_logfile: joi.string().default(null),
+	stderr_logfile: joi.string().default(null),
+	umask: joi.string().regex(/^[0-7]{1,3}$/).default('022'),
+	autorestart: joi.string().valid(['always', 'never', 'unexpected']).default('unexpected'),
+	startretries: joi.number().integer().min(0).default(3),
+	directory: joi.string().default(null),
+	startsecs: joi.number().integer().min(0).default(1),
+	exitcodes: joi.alternatives().try(joi.array().items(joi.number().integer()), joi.number().integer()).default([0, 2]),
+	env: joi.object().default(null),
+	stopsignal: joi.number().integer().valid([
+		signalCode.SIGTERM,
+		signalCode.SIGINT,
+		signalCode.SIGQUIT,
+		signalCode.SIGHUP,
+		signalCode.SIGUSR2
+	]).default(signalCode.SIGTERM)
+})
 
 module.exports = (filePath) => {
 	return new Promise((resolve, reject) => {
 		jsonfile.readFile(filePath, (err, config) => {
-			if (err) reject(err)
+			if (err) return reject(err)
 
-			for (const processName in config) {
-				if (typeof config[processName].command !== 'string' || config[processName].command.length === 0) reject(err)
-				if (typeof config[processName].numprocs !== 'number' || config[processName].numprocs < 1) reject(err)
-				if (typeof config[processName].directory !== 'string' || config[processName].directory.length === 0) reject(err)
-				if (typeof config[processName].autostart !== 'boolean') reject(err)
-				if (
-					typeof config[processName].autorestart !== 'string' ||
-					(config[processName].autorestart !== 'always'&&
-					config[processName].autorestart !== 'never' &&
-					config[processName].autorestart !== 'unexpected')
-				) reject(err)
-				if (Array.isArray(config[processName].exitcodes) && config[processName].exitcodes.length > 0) {
-					config[processName].exitcodes.forEach(element => { if (typeof element !== 'number') reject(err) })
-				}
-				else if (typeof config[processName].exitcodes !== 'number') reject(err)
-				if (typeof config[processName].startretries !== 'number' && config[processName].startretries > -1) reject(err)
-				if (typeof config[processName].startsecs !== 'number') reject(err)
-				if (
-					typeof config[processName].stopsignal !== 'number' ||
-					![15, 1, 2, 3, 4, 30, 31].includes(config[processName].stopsignal)
-				) reject(err)
-				if (typeof config[processName].stopwaitsecs !== 'number' && config[processName].stopwaitsecs > -1) reject(err)
-				if (typeof config[processName].stderr !== 'string') reject(err)
-				if (typeof config[processName].stdout !== 'string') reject(err)
-				if (typeof config[processName].env !== 'object') reject(err)
-				if (typeof config[processName].umask !== 'string' || parseInt(config[processName].umask) < 0 || parseInt(config[processName].umask) > 777) reject(err)
-			}
-			resolve(config)
+			Promise.all(Object.keys(config).map((processName) => joi.validate(config[processName], schema)))
+			.then((configParsed) => resolve(configParsed))
+			.catch((err) => reject(err))
 		})
 	})
 }
