3816b7b95b728d9dfb343e85713b1d2861710c08
mimazouz <mimazouz@student.42.fr>
2018-03-07 15:04:48 +0100

    fix bg start

--- src/client/controller.js
+++ src/client/controller.js
@@ -4,17 +4,13 @@ const help = require('./help')
 const logger = require('../services/logger')
 
 module.exports = {
-	'status': (argv, client) => {
-		client.write(JSON.stringify(argv))
-	},
-	'start': (argv, client) => {
-		argv.splice(0, 1)
-
-		if (argv[0])
-			client.write(JSON.stringify(argv))
+	'status': (argv) => argv,
+	'start': (argv) => {
+		if (argv[1]) return argv
 		else {
 			console.error("error: start requires a process name")
 			help["start"]()
+			return null
 		}
 	},
 	'stop': (argv, client) => {
