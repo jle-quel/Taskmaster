e8637fd8af5174aa49ccd15759ef19f65a80fc29
mimazouz <mimazouz@student.42.fr>
2018-03-10 23:51:12 +0100

    stopwaitsecs

--- src/server/controllers/stop.js
+++ src/server/controllers/stop.js
@@ -22,8 +22,15 @@ const all = () => {
         processCopy.send(_process)
         _process['process'] = processCopy
         childProcess.spawn(`kill -${_process.config.stopsignal} ${_process.pid}`, [], {detached: true, shell: true})
-        stop.push(`${processGroupLength === 1 ? '' : processGroupName + ':'}${processName}: STOPPED`)
-	    	logger.write("INFO", `stopped [${processName}] (terminated by ${_process.config.stopsignal})`)
+        childProcess.exec(`kill -0 ${_process.pid}`, (err, stdout, stderr) => {
+          if (stderr === '') {
+            logger.write("INFO", `Waiting for the ${processName} with pid: ${_process.pid} to stop`)
+            timeout(_process.pid, _process.config.stopwaitsecs)
+          } else {
+            stop.push(`${processGroupLength === 1 ? '' : processGroupName + ':'}${processName}: STOPPED`)
+            logger.write("INFO", `stopped [${processName}] (terminated by ${_process.config.stopsignal})`)
+          }
+        })
       } else stop.push(`${processName + ':'} ERROR (already stopped)`)
     })
   })
@@ -47,8 +54,15 @@ const one = (processNamesOrGroupName) => {
 
           processCopy.send(_process)
           childProcess.spawn(`kill -${_process.config.stopsignal} ${_process.pid}`, [], {detached: true, shell: true})
-          stop.push(`${processGroupLength === 1 ? '' : processNameOrGroupName + ':'}${processName}: STOPPED`)
-		      logger.write("INFO", `stopped [${processName}] (terminated by ${_process.config.stopsignal})`)
+          childProcess.exec(`kill -0 ${_process.pid}`, (err, stdout, stderr) => {
+            if (stderr === '') {
+              logger.write("INFO", `Waiting for the ${processName} with pid: ${_process.pid} to stop`)
+              timeout(_process.pid, _process.config.stopwaitsecs)
+            } else {
+              stop.push(`${processGroupLength === 1 ? '' : processNameOrGroupName + ':'}${processName}: STOPPED`)
+              logger.write("INFO", `stopped [${processName}] (terminated by ${_process.config.stopsignal})`)
+            }
+          })
         } else stop.push(`${processName + ':'} ERROR (already stopped)`)
       })
     } else {
@@ -57,19 +71,33 @@ const one = (processNamesOrGroupName) => {
       if (processInfos) {
         const processDataFound = processInfos[1]
 
-      if (_process.status === 'RUNNING' || _process.status === 'STARTING') {
+      if (processDataFound.status === 'RUNNING' || processDataFound.status === 'STARTING') {
           processDataEdit({killedByMe: true}, processInfos[0], processNameOrGroupName)
           const processCopy = processDataFound['process']
           delete processDataFound['process']
 
           processCopy.send(processDataFound)
           childProcess.spawn(`kill -${processDataFound.config.stopsignal} ${processDataFound.pid}`, [], {detached: true, shell: true})
-          stop.push(`${processNameOrGroupName}: STOPPED`)
+          childProcess.exec(`kill -0 ${processDataFound.pid}`, (err, stdout, stderr) => {
+            if (stderr === '') {
+              logger.write("INFO", `Waiting for the ${processNameOrGroupName} with pid: ${processDataFound.pid} to stop`)
+              timeout(processDataFound.pid, processDataFound.config.stopwaitsecs)
+            } else {
+              stop.push(`${processNameOrGroupName}: STOPPED`)
+              logger.write("INFO", `stopped [${processNameOrGroupName}] (terminated by ${processDataFound.config.stopsignal})`)
+            }
+          })
         } else stop.push(`${processNameOrGroupName + ':'} ERROR (already stopped)`)
       } else stop.push(`${processNameOrGroupName}: ERROR (no such process)`)
     }
   })
   return Promise.resolve(stop.join('\n'))
+}
+
+const timeout = (pid, stopwaitsecs) => {
+  return setTimeout(() => {
+    childProcess.spawn(`kill -9 ${pid}`, [], {detached: true, shell: true})
+  }, stopwaitsecs * 1000)
 }
 
 module.exports = {
